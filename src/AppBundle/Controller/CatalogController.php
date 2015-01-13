4a5b823c401f0c4b56503a1b9e333a7006ce5a96
jackjeka <angie.or.jackie@gmail.com>
2015-01-13 20:14:09 +0200

    add catalog updating

--- src/AppBundle/Controller/CatalogController.php
+++ src/AppBundle/Controller/CatalogController.php
@@ -2,12 +2,11 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Catalog;
-use AppBundle\Entity\Item;
 use AppBundle\Form\Type\CatalogType;
-use AppBundle\Form\Type\ItemType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template as Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -23,16 +22,16 @@ class CatalogController extends Controller
     public function showAction(Catalog $catalog)
     {
         return [
-            'catalog'=>$catalog
+            'catalog' => $catalog
         ];
     }
 
     /**
      * @Route("/create-catalog", name="create-catalog")
      * @param  Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @return RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function createCatalogAction(Request $request)
+    public function createAction(Request $request)
     {
         $catalog = new Catalog();
         $form = $this->createForm(new CatalogType(), $catalog);
@@ -50,4 +49,26 @@ class CatalogController extends Controller
             ]);
     }
 
+    /**
+     * @Route("/catalogs/{id}/update", name="catalog_update")
+     * @Template()
+     * @param Request $request
+     * @param $catalog
+     * @return array|RedirectResponse
+     */
+    public function updateAction(Request $request, Catalog $catalog, $id)
+    {
+        $form = $this->createForm(new CatalogType(), $catalog);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $this->getDoctrine()
+                ->getManager()
+                ->flush();
+            return $this->redirectToRoute('catalog', array('id' => $id), 301);
+        }
+        return [
+            'catalog' => $catalog,
+            'form' => $form->createView(),
+        ];
+    }
 }
